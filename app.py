9e903a7763f5d5cded28c3790b1b6dd15907c8e8
Mahidhar Reddy CH <mahidhar@thesmartbridge.com>
2023-07-11 21:43:01 +0530

    added all files with registration

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import ibm_db
 app = Flask(__name__)
 
-@app.route("/")
+conn = conn=ibm_db.connect("DATABASE=database;HOSTNAME=hostname;PORT=port;UID=username;PWD=password;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt",'','')
+print(conn)
+connState = ibm_db.active(conn)
+print(connState)
 def index():
     return render_template("index.html")
 
@@ -23,6 +27,30 @@ def login():
 
 @app.route("/profile")
 def profile():
+    return render_template("profile.html")
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if request.method == "POST":
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        role = request.form['role']
+        details = [name,email,password,role]
+        print(details)
+        sql = "SELECT * FROM REGISTER_HC where EMAILID=?"
+        stmt = ibm_db.prepare(conn, sql)
+        ibm_db.bind_param(stmt, 1, email)
+        # ibm_db.bind_param(stmt, 2, name)
+        ibm_db.execute(stmt)
+        acc = ibm_db.fetch_assoc(stmt)
+        print(acc)
+        if acc:
+            print("alr")
+            return render_template("login.html")
+        else: 
+            #"code for insert values in to  "
+            return render_template("profile.html")
     return render_template("profile.html")
 
 
