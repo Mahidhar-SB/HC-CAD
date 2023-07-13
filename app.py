e2d07d8738d7548886486f241be8f1337dd96f29
Mahidhar Reddy CH <mahidhar@thesmartbridge.com>
2023-07-13 21:16:14 +0530

    pushed all the files along with docker

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import ibm_db
 app = Flask(__name__)
 
-conn = conn=ibm_db.connect("DATABASE=database;HOSTNAME=hostname;PORT=port;UID=username;PWD=password;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt",'','')
+conn = conn=ibm_db.connect("DATABASE=bludb;HOSTNAME=2f3279a5-73d1-4859-88f0-a6c3e6b4b907.c3n41cmd0nqnrk39u98g.databases.appdomain.cloud;PORT=30756;UID=vts47207;PWD=g4O8mWodzytk1wMD;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt",'','')
 print(conn)
 connState = ibm_db.active(conn)
 print(connState)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+
+@app.route("/")
 def index():
     return render_template("index.html")
 
@@ -21,8 +25,35 @@ def jobpost():
 def jobview():
     return render_template("job-view.html")
 
-@app.route("/login")
+@app.route("/login", methods = ['GET', 'POST'])
 def login():
+    global uemail
+    if request.method == 'POST':
+        email = request.form['email']
+        password =  request.form['password']
+        details = [email, password]
+        print(details)
+        sql = "SELECT * FROM REGISTER_HC where EMAILID=? AND PASSWORD = ?"
+        stmt = ibm_db.prepare(conn, sql)
+        ibm_db.bind_param(stmt, 1, email)
+        ibm_db.bind_param(stmt, 2, password)
+        ibm_db.execute(stmt)
+        acc = ibm_db.fetch_assoc(stmt)
+        print(acc)
+        if acc: 
+            session['email'] = email
+            session['username'] = acc['NAME']
+            uemail = session['email']            
+            return render_template("profile.html")
+        else:
+            msg = "Invalid Credentials"
+            return render_template("login.html", message=msg)
+
+
+
+
+
+
     return render_template("login.html")
 
 @app.route("/profile")
@@ -46,12 +77,26 @@ def register():
         acc = ibm_db.fetch_assoc(stmt)
         print(acc)
         if acc:
-            print("alr")
-            return render_template("login.html")
+            msg = "You have been already REGISTERED, please login!"
+            return render_template("login.html", message = msg)
         else: 
-            #"code for insert values in to  "
-            return render_template("profile.html")
-    return render_template("profile.html")
+            sql = "INSERT into REGISTER_HC VALUES (?,?,?,?)"
+            stmt = ibm_db.prepare(conn, sql)
+            ibm_db.bind_param(stmt, 1, name)
+            ibm_db.bind_param(stmt, 2, email)
+            ibm_db.bind_param(stmt, 3, password)
+            ibm_db.bind_param(stmt, 4, role)
+            ibm_db.execute(stmt)
+            msg = "You have Successfully REGISTERED, Please LOGIN"            
+            return render_template("login.html", message = msg)
+    return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    session.pop("email", None)
+    session.pop("username", None)
+    return render_template("index.html")
+
 
 
 
